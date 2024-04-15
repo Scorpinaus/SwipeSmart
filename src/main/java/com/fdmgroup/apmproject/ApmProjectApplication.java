f5022ea87d7ec2d1c9eeef1b443f4101945d4a5a
corwin yip <corwin.yip@fdmgroup.com>
2024-04-15 03:21:11 +0000

    Update file ApmProjectApplication.java

--- src/main/java/com/fdmgroup/apmproject/ApmProjectApplication.java
+++ src/main/java/com/fdmgroup/apmproject/ApmProjectApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApmProjectApplication {
-	
-	
-	
-	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(ApmProjectApplication.class, args);
 	}

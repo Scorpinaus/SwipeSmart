41551e1961017001b407bcca9d71aa116ade3808
jacky tan1 <jacky.tan1@fdmgroup.com>
2024-04-18 12:52:06 +0800

    Created paybills page, getMapping without functionality

--- src/main/java/com/fdmgroup/apmproject/controller/TransactionController.java
+++ src/main/java/com/fdmgroup/apmproject/controller/TransactionController.java
@@ -3,17 +3,14 @@ package com.fdmgroup.apmproject.controller;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 

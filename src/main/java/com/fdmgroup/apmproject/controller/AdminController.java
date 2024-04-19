4d3679ab706e7449b0bc4b7c86f953b6792d7f50
corwin yip <corwin.yip@fdmgroup.com>
2024-04-19 23:26:43 +0800

    add role validation in the admin controller

--- src/main/java/com/fdmgroup/apmproject/controller/AdminController.java
+++ src/main/java/com/fdmgroup/apmproject/controller/AdminController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +27,7 @@ import com.fdmgroup.apmproject.service.UserService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
 	@Autowired

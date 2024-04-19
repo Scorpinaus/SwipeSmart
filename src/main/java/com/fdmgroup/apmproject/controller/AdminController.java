46c3df669087548ee66dc1b103e1287f66439a9b
bryant pang <bryant.pang@fdmgroup.com>
2024-04-19 11:42:02 +0800

    Made changes to AdminController class to filter transactions

--- src/main/java/com/fdmgroup/apmproject/controller/AdminController.java
+++ src/main/java/com/fdmgroup/apmproject/controller/AdminController.java
@@ -140,7 +140,8 @@ public class AdminController {
 
 	@PostMapping("/admin/transactions")
 	public String adminViewAllTransactions(@RequestParam(name = "month", required = false) String month,
-			@RequestParam(name = "pickedUser", required = false) String pickedUser, Model model, HttpSession session) {
+			@RequestParam(name = "pickedUser", required = false) String pickedUser,
+			@RequestParam(name = "pickedType", required = false) String pickedType, Model model, HttpSession session) {
 		User loggedUser = (User) session.getAttribute("loggedUser");
 		model.addAttribute("user", loggedUser);
 		List<User> userList = userService.findAllUsers();
@@ -148,48 +149,129 @@ public class AdminController {
 		List<CreditCard> creditCards = creditCardService.findAllCreditCards();
 		List<Account> accountList = accountService.getAllAccounts();
 		if (month == null || month == "") {
-			for (CreditCard cc : creditCards) {
-				List<Transaction> transaction = cc.getTransactions();
-				for (Transaction tx : transaction) {
-					if (tx.getTransactionCreditCard().getCreditCardUser().getUsername().equals(pickedUser)) {
-						transactions.add(tx);
+			if (pickedType == null) {
+				if (pickedUser != null) {
+					for (CreditCard cc : creditCards) {
+						List<Transaction> transaction = cc.getTransactions();
+						for (Transaction tx : transaction) {
+							if (tx.getTransactionCreditCard().getCreditCardUser().getUsername().equals(pickedUser)) {
+								transactions.add(tx);
+							}
+						}
+					}
+					for (Account a : accountList) {
+						List<Transaction> transaction = a.getTransactions();
+						for (Transaction tx : transaction) {
+							if (tx.getTransactionAccount().getAccountUser().getUsername().equals(pickedUser)) {
+								transactions.add(tx);
+							}
+						}
+					}
+				} else {
+					for (CreditCard cc : creditCards) {
+						List<Transaction> transaction = cc.getTransactions();
+						for (Transaction tx : transaction) {
+							transactions.add(tx);
+						}
+					}
+					for (Account a : accountList) {
+						List<Transaction> transaction = a.getTransactions();
+						for (Transaction tx : transaction) {
+							transactions.add(tx);
+						}
+					}
+				}
+			} else if (pickedType.equals("card")) {
+				for (CreditCard cc : creditCards) {
+					List<Transaction> transaction = cc.getTransactions();
+					for (Transaction tx : transaction) {
+						if (tx.getTransactionCreditCard().getCreditCardUser().getUsername().equals(pickedUser)) {
+							transactions.add(tx);
+						}
+					}
+				}
+			} else if (pickedType.equals("account")) {
+				for (Account a : accountList) {
+					List<Transaction> transaction = a.getTransactions();
+					for (Transaction tx : transaction) {
+						if (tx.getTransactionAccount().getAccountUser().getUsername().equals(pickedUser)) {
+							transactions.add(tx);
+						}
 					}
 				}
 			}
-			for (Account a : accountList) {
-				List<Transaction> transaction = a.getTransactions();
-				for (Transaction tx : transaction) {
-					if (tx.getTransactionAccount().getAccountUser().getUsername().equals(pickedUser)) {
-						transactions.add(tx);
-					}
-				}
-			}
-			Collections.sort(transactions, Comparator.comparing(Transaction::getTransactionDate));
 		} else {
 			int year = Integer.parseInt(month.substring(0, 4));
 			int monthValue = Integer.parseInt(month.substring(5, 7));
-			for (CreditCard cc : creditCards) {
-				List<Transaction> transaction = cc.getTransactions();
-				for (Transaction t : transaction) {
-					if ((t.getTransactionDate().getMonthValue() == monthValue
-							&& t.getTransactionDate().getYear() == year)
-							&& (t.getTransactionCreditCard().getCreditCardUser().getUsername().equals(pickedUser))) {
-						transactions.add(t);
+			if (pickedType == null) {
+				if (pickedUser != null) {
+					for (CreditCard cc : creditCards) {
+						List<Transaction> transaction = cc.getTransactions();
+						for (Transaction t : transaction) {
+							if ((t.getTransactionDate().getMonthValue() == monthValue
+									&& t.getTransactionDate().getYear() == year)
+									&& (t.getTransactionCreditCard().getCreditCardUser().getUsername()
+											.equals(pickedUser))) {
+								transactions.add(t);
+							}
+						}
+					}
+					for (Account a : accountList) {
+						List<Transaction> transaction = a.getTransactions();
+						for (Transaction t : transaction) {
+							if ((t.getTransactionDate().getMonthValue() == monthValue
+									&& t.getTransactionDate().getYear() == year)
+									&& (t.getTransactionAccount().getAccountUser().getUsername().equals(pickedUser))) {
+								transactions.add(t);
+							}
+						}
+					}
+				} else {
+					for (CreditCard cc : creditCards) {
+						List<Transaction> transaction = cc.getTransactions();
+						for (Transaction t : transaction) {
+							if (t.getTransactionDate().getMonthValue() == monthValue
+									&& t.getTransactionDate().getYear() == year) {
+								transactions.add(t);
+							}
+						}
+					}
+					for (Account a : accountList) {
+						List<Transaction> transaction = a.getTransactions();
+						for (Transaction t : transaction) {
+							if (t.getTransactionDate().getMonthValue() == monthValue
+									&& t.getTransactionDate().getYear() == year) {
+								transactions.add(t);
+							}
+						}
+					}
+				}
+			} else if (pickedType.equals("card")) {
+				for (CreditCard cc : creditCards) {
+					List<Transaction> transaction = cc.getTransactions();
+					for (Transaction t : transaction) {
+						if ((t.getTransactionDate().getMonthValue() == monthValue
+								&& t.getTransactionDate().getYear() == year)
+								&& (t.getTransactionCreditCard().getCreditCardUser().getUsername()
+										.equals(pickedUser))) {
+							transactions.add(t);
+						}
+					}
+				}
+			} else if (pickedType.equals("account")) {
+				for (Account a : accountList) {
+					List<Transaction> transaction = a.getTransactions();
+					for (Transaction t : transaction) {
+						if ((t.getTransactionDate().getMonthValue() == monthValue
+								&& t.getTransactionDate().getYear() == year)
+								&& (t.getTransactionAccount().getAccountUser().getUsername().equals(pickedUser))) {
+							transactions.add(t);
+						}
 					}
 				}
 			}
-			for (Account a : accountList) {
-				List<Transaction> transaction = a.getTransactions();
-				for (Transaction t : transaction) {
-					if ((t.getTransactionDate().getMonthValue() == monthValue
-							&& t.getTransactionDate().getYear() == year)
-							&& (t.getTransactionAccount().getAccountUser().getUsername().equals(pickedUser))) {
-						transactions.add(t);
-					}
-				}
-			}
-			Collections.sort(transactions, Comparator.comparing(Transaction::getTransactionDate));
 		}
+		Collections.sort(transactions, Comparator.comparing(Transaction::getTransactionDate));
 		model.addAttribute("users", userList);
 		model.addAttribute("transactions", transactions);
 		return "admintransactions";

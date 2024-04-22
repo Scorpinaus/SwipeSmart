d2873456d225348018f36842a973308159e75ad4
jacky tan1 <jacky.tan1@fdmgroup.com>
2024-04-22 11:17:46 +0800

    Completed TransactionControllerTest class

--- src/test/java/com/fdmgroup/apmproject/controller/TransactionControllerTest.java
+++ src/test/java/com/fdmgroup/apmproject/controller/TransactionControllerTest.java
@@ -1,10 +1,18 @@
 package com.fdmgroup.apmproject.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.ui.Model;
 
 import com.fdmgroup.apmproject.model.Account;
+import com.fdmgroup.apmproject.model.CreditCard;
 import com.fdmgroup.apmproject.model.Transaction;
 import com.fdmgroup.apmproject.model.User;
 import com.fdmgroup.apmproject.service.AccountService;
@@ -28,8 +37,6 @@ public class TransactionControllerTest {
 	
 	@Autowired
 	private UserService userService;
-	@Autowired
-	private CreditCardService creditCardService;
 	@Autowired
 	private AccountService accountService;
 	@Autowired
@@ -60,7 +67,7 @@ public class TransactionControllerTest {
     }
 	
     @Test
-    void testViewCardTransactions_AccountTransactions() {
+    void testViewTransactions_AccountTransactions() {
         // Arrange
         User loggedUser = userService.findUserByUsername("jackytan"); 
         when(session.getAttribute("loggedUser")).thenReturn(loggedUser);
@@ -75,9 +82,80 @@ public class TransactionControllerTest {
         // Assert
         assertEquals("view-transactions", viewName);
         verify(model).addAttribute("user", loggedUser);
-        verify(model).addAttribute("account", mockAccount);
-        verify(model).addAttribute("transactions", mockTransactions);
+        when(model.getAttribute("account")).thenReturn(userAccount.get(0));
+        when(model.getAttribute("transactions")).thenReturn(accountTransactions);
     }
 	
+    @Test
+    void testViewTransactions_CreditCardTransactions() {
+        // Arrange
+        User loggedUser = userService.findUserByUsername("jackytan"); 
+        when(session.getAttribute("loggedUser")).thenReturn(loggedUser);
+        
+        List<CreditCard> userCreditCard = loggedUser.getCreditCards();
+        List<Transaction> creditCardTransactions = userCreditCard.get(0).getTransactions();
+        
+        
+        // Act
+        String viewName = transactionController.viewCardTransactions(null, String.valueOf(userCreditCard.get(0).getCreditCardId()), null, model, session);
+        
+        // Assert
+        assertEquals("view-transactions", viewName);
+        verify(model).addAttribute("user", loggedUser);
+        when(model.getAttribute("creditCard")).thenReturn(userCreditCard.get(0));
+        when(model.getAttribute("transactions")).thenReturn(creditCardTransactions);
+    }
+    
+    @Test
+    void testViewTransactionsMonthFilterCreditCard() {
+        // Arrange
+        User loggedUser = userService.findUserByUsername("jackytan"); 
+        when(session.getAttribute("loggedUser")).thenReturn(loggedUser);
+        
+        List<CreditCard> userCreditCard = loggedUser.getCreditCards();
+        String month = "2024-04";
+        
+        
+        // Act
+        String viewName = transactionController.viewCardTransactions(month, String.valueOf(userCreditCard.get(0).getCreditCardId()), null, model, session);
+        int year = Integer.parseInt(month.substring(0, 4));
+		int monthValue = Integer.parseInt(month.substring(5));
+		List<Transaction> expectedTransactions = transactionService.getTransactionsByMonthAndYearAndTransactionCreditCard(year,
+				monthValue, userCreditCard.get(0));
+		Collections.sort(expectedTransactions, Comparator.comparing(Transaction::getTransactionDate));
+        
+        // Assert
+        assertEquals("view-transactions", viewName);
+        verify(model).addAttribute("user", loggedUser);
+        when(model.getAttribute("creditCard")).thenReturn(userCreditCard.get(0));
+        when(model.getAttribute("transactions")).thenReturn(expectedTransactions);
+    }
+    
+    @Test
+    void testViewTransactionsMonthFilterAccount() {
+        // Arrange
+        User loggedUser = userService.findUserByUsername("jackytan"); 
+        when(session.getAttribute("loggedUser")).thenReturn(loggedUser);
+        
+        List<Account> userAccount = accountService.findAllAccountsByUserId(loggedUser.getUserId());
+        String month = "2024-04";
+        
+        
+        // Act
+        String viewName = transactionController.viewCardTransactions(month, String.valueOf(userAccount.get(0).getAccountId()), null, model, session);
+        int year = Integer.parseInt(month.substring(0, 4));
+		int monthValue = Integer.parseInt(month.substring(5));
+		List<Transaction> expectedTransactions = transactionService.getTransactionsByMonthAndYearAndTransactionAccount(year,
+				monthValue, userAccount.get(0));
+		Collections.sort(expectedTransactions, Comparator.comparing(Transaction::getTransactionDate));
+        
+        // Assert
+        assertEquals("view-transactions", viewName);
+        verify(model).addAttribute("user", loggedUser);
+        when(model.getAttribute("creditCard")).thenReturn(userAccount.get(0));
+        when(model.getAttribute("transactions")).thenReturn(expectedTransactions);
+    }
+    
+
 	
 }

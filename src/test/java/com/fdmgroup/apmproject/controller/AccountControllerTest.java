9f7d58419333c684bfdc19d18393ea466b2f4a7c
Hafiz Saffie <hafiz.saffie@fdmgroup.com>
2024-04-23 11:04:04 +0800

    Added accountControlleerTest unit cases

--- src/test/java/com/fdmgroup/apmproject/controller/AccountControllerTest.java
+++ src/test/java/com/fdmgroup/apmproject/controller/AccountControllerTest.java
@@ -1,15 +1,18 @@
 package com.fdmgroup.apmproject.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,6 +27,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fdmgroup.apmproject.model.Account;
 import com.fdmgroup.apmproject.model.ForeignExchangeCurrency;
+import com.fdmgroup.apmproject.model.Status;
+import com.fdmgroup.apmproject.model.Transaction;
 import com.fdmgroup.apmproject.model.User;
 import com.fdmgroup.apmproject.service.AccountService;
 import com.fdmgroup.apmproject.service.ForeignExchangeCurrencyService;
@@ -358,7 +363,223 @@ public class AccountControllerTest {
 		assertEquals("create-bank-account", viewName);
 	}
 	
+	@Test
+	@DisplayName("Test createBankAccount function for successful account creation")
+	void testCreateBankAccountOne() {
+		//Arrange
+		String accountName = "Savings Account";
+		double initialDeposit = 6000.0;
+		User currentUser = user;
+		session.setAttribute("loggerUser", currentUser);
+		Account accountOne = account;
+		accountOne.setAccountName(accountName);
+		accountOne.setBalance(initialDeposit);
+		accountOne.setAccountNumber("1234567890");
+		accountOne.setAccountUser(currentUser);
+		accountOne.setAccountStatus(new Status("Pending"));
+		
+		when(currencyService.getCurrencyByCode("SGD")).thenReturn(foreignCurrency);
+		when(statusService.findByStatusName("Pending")).thenReturn(new Status("Pending"));
+		when(accountService.generateUniqueAccountNumber()).thenReturn("1234567890");
+		
+		//Act
+		String viewName = accountController.createBankAccount(accountName, initialDeposit, session, redirectAttributes);
+		
+		//Assert
+		assertEquals("redirect:/bankaccount/dashboard", viewName);
+		verify(accountService).persist(accountOne);
+		verify(transactionService).persist(any(Transaction.class));
+	}
 	
+	@Test
+	@DisplayName("Test createBankAccount function for insufficient initial deposit")
+	void testCreateBankAccountTwo() {
+		//Arrange
+		String accountName = "Savings Account";
+		double initialDeposit = 50.0;
+		
+		//Act
+		String viewName = accountController.createBankAccount(accountName, initialDeposit, session, redirectAttributes);
+		
+		//Assert
+		assertEquals("redirect:/bankaccount/create", viewName);
+		verify(redirectAttributes).addAttribute("InsufficientInitialDepositError", "true");
+		verifyNoInteractions(accountService);
+	}
 	
+	@Test
+	@DisplayName("Test createBankAccount function for Empty or blank account name")
+	void testCreateBankAccountThree() {
+		//Arrange
+		String accountName = " ";
+		double initialDeposit = 150.0;
+		
+		//Act
+		String viewName = accountController.createBankAccount(accountName, initialDeposit, session, redirectAttributes);
+		
+		//Assert
+		assertEquals("redirect:/bankaccount/create", viewName);
+		verifyNoInteractions(accountService);
+	}
 	
+	@Test
+	@DisplayName("Test to go for transfer page for successful page load")
+	void testGoToTransferPageOne() {
+		//Arrange
+		User currentUser = user;
+		currentUser.setUserId(1L);
+		session.setAttribute("loggedUser", currentUser);
+		Account accountOne = account;
+		Account accountTwo = account;
+		List<Account> accountList = new ArrayList<>();
+		accountList.add(accountOne);
+		accountList.add(accountTwo);
+		ForeignExchangeCurrency currencyUSD = foreignCurrency;
+		currencyUSD.setCode("USD");
+		ForeignExchangeCurrency currencyEUR = foreignCurrency;
+		currencyUSD.setCode("EUR");
+		List<ForeignExchangeCurrency> currencyList = new ArrayList<>();
+		currencyList.add(currencyUSD);
+		currencyList.add(currencyEUR);
+		
+		when(accountService.findAllAccountsByUserId(currentUser.getUserId())).thenReturn(accountList);
+       when(currencyService.getSupportedCurrencies()).thenReturn(currencyList);
+        
+        //Act
+        String viewName = accountController.goToTransferPage(model, session);
+        
+        //Assert
+       assertEquals("transfer", viewName);
+       verify(model).addAttribute("AccountList", accountList);		
+	}
+	
+	@Test
+	@DisplayName("Test bankAccount transfer function to same account")
+	void testBankAccountTransferOne() {
+		//Arrange
+		String accountNumber = "111111111";
+		Account accountOne = account;
+		accountOne.setAccountNumber(accountNumber);
+		accountOne.setAccountId(1L);
+		accountOne.setBalance(100.0);
+		accountOne.setCurrencyCode("USD");
+		when(accountService.findById(accountOne.getAccountId())).thenReturn(accountOne);
+		when(currencyService.getExchangeRate("USD", "USD")).thenReturn(BigDecimal.ONE);
+		
+		
+		//Act
+		String result = accountController.transferMoney(accountOne.getAccountId(), 100.0, accountNumber, "USD", session, redirectAttributes);
+		
+		//Assert
+		assertEquals("redirect:/bankaccount/transfer", result);
+		verify(redirectAttributes).addAttribute("SameAccountError", "true");
+	}
+	
+	@Test
+	@DisplayName("Test bankAccount transfer function for insufficient funds")
+	void testBankAccountTransferTwo() {
+		//Arrange
+		String accountNumber = "111111111";
+		Account accountOne = account;
+		accountOne.setAccountNumber(accountNumber);
+		accountOne.setAccountId(1L);
+		accountOne.setBalance(100.0);
+		accountOne.setCurrencyCode("USD");
+		when(accountService.findById(accountOne.getAccountId())).thenReturn(accountOne);
+		when(currencyService.getExchangeRate("USD", "USD")).thenReturn(BigDecimal.ONE);
+		
+		//Act
+		String result = accountController.transferMoney(1L, 1000.0, "987654321", "USD", session, redirectAttributes);
+
+		
+		//Assert
+		assertEquals("redirect:/bankaccount/transfer", result);
+		verify(redirectAttributes).addAttribute("InsufficientBalanceError", "true");
+		
+	}
+	
+	@Test
+	@DisplayName("Test bankAccount Transfer for successful internal transfer")
+	void testBankAccountTransferThree() {
+		//Arrange
+		Account accountOne = new Account();
+        accountOne.setAccountNumber("111111111");
+        accountOne.setAccountId(1L);
+        accountOne.setBalance(1000.0);
+        accountOne.setCurrencyCode("USD");
+				
+        Account accountTwo = new Account();
+        accountTwo.setAccountNumber("999999999");
+        accountTwo.setAccountId(2L);
+        accountTwo.setBalance(1000.0);
+        accountTwo.setCurrencyCode("USD");
+        accountTwo.setAccountStatus(new Status("Active"));
+				        when(accountService.findById(accountOne.getAccountId())).thenReturn(accountOne);
+        when(currencyService.getExchangeRate("USD", "USD")).thenReturn(BigDecimal.ONE);
+        when(accountService.findAccountByAccountNumber(accountTwo.getAccountNumber())).thenReturn(accountTwo);
+        double transferAmount = 100.0;
+				
+				//Act
+				String result = accountController.transferMoney(accountOne.getAccountId(), transferAmount, accountTwo.getAccountNumber(), "USD", session, redirectAttributes);
+
+				
+				//Assert
+				assertEquals("redirect:/bankaccount/dashboard", result);
+	}
+	
+	@Test
+	@DisplayName("Test bankAccount transfer to pending Account")
+	void testBankAccountTransferFour() {
+	//Arrange
+		Account accountOne = new Account();
+        accountOne.setAccountNumber("111111111");
+        accountOne.setAccountId(1L);
+        accountOne.setBalance(1000.0);
+        accountOne.setCurrencyCode("USD");
+        accountOne.setAccountStatus(new Status("Active"));
+		Status pendingStatus = new Status("Pending");
+        Account accountTwo = new Account();
+        accountTwo.setAccountNumber("999999999");
+        accountTwo.setAccountId(2L);
+        accountTwo.setBalance(1000.0);
+        accountTwo.setCurrencyCode("USD");
+        accountTwo.setAccountStatus(pendingStatus);
+        
+				        when(accountService.findById(accountOne.getAccountId())).thenReturn(accountOne);
+        when(currencyService.getExchangeRate("USD", "USD")).thenReturn(BigDecimal.ONE);
+        when(accountService.findAccountByAccountNumber(accountTwo.getAccountNumber())).thenReturn(accountTwo);
+        when(statusService.findByStatusName("Pending")).thenReturn(pendingStatus);
+        double transferAmount = 100.0;
+	
+	//Act
+	String result = accountController.transferMoney(accountOne.getAccountId(), transferAmount, accountTwo.getAccountNumber(), "USD", session, redirectAttributes);
+        
+	//Assert
+	assertEquals("redirect:/bankaccount/transfer", result);
+	verify(redirectAttributes).addAttribute("RecipientAccountPendingError", "true");
+	}
+	
+	@Test
+	@DisplayName("Test bankAccount transfer for external transfer")
+	void testBankAccountTransferFive() {
+		//Arrange
+		Account accountOne = new Account();
+        accountOne.setAccountNumber("111111111");
+        accountOne.setAccountId(1L);
+        accountOne.setBalance(1000.0);
+        accountOne.setCurrencyCode("USD");
+        accountOne.setAccountStatus(new Status("Active"));
+				        when(accountService.findById(accountOne.getAccountId())).thenReturn(accountOne);
+        when(currencyService.getExchangeRate("USD", "USD")).thenReturn(BigDecimal.ONE);
+        when(accountService.findAccountByAccountNumber("external-1")).thenReturn(null);
+        double transferAmount = 100.0;
+		
+		//Act
+        String result = accountController.transferMoney(accountOne.getAccountId(), transferAmount, "external-1", "USD", session, redirectAttributes);
+        
+		
+		//Assert
+        assertEquals("redirect:/bankaccount/dashboard", result);
+        verify(transactionService).persist(any(Transaction.class));
+	}
 }

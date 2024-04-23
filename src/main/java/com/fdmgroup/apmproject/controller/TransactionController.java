15727d5e8618056eaf228cf09ee96382caa1610d
bryant pang <bryant.pang@fdmgroup.com>
2024-04-23 13:58:44 +0800

    Added model attribute to transactioncontroller class

--- src/main/java/com/fdmgroup/apmproject/controller/TransactionController.java
+++ src/main/java/com/fdmgroup/apmproject/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.fdmgroup.apmproject.controller;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -27,10 +28,11 @@ import com.fdmgroup.apmproject.service.UserService;
 import jakarta.servlet.http.HttpSession;
 
 /**
- * This class is a Spring MVC controller responsible for handling requests related to transactions.
- * It provides methods for viewing transactions for credit cards and accounts.
+ * This class is a Spring MVC controller responsible for handling requests
+ * related to transactions. It provides methods for viewing transactions for
+ * credit cards and accounts.
  *
- * @author 
+ * @author
  * @version 1.0
  * @since 2024-04-22
  */
@@ -49,21 +51,23 @@ public class TransactionController {
 	private static Logger logger = LogManager.getLogger(CreditCardController.class);
 
 	private List<ForeignExchangeCurrency> currencies;
-	
-	
-	 /**
-     * This method displays a list of transactions for a given credit card or account.
-     *
-     * @param month           The month for which to filter transactions (optional).
-     * @param creditCardId    The ID of the credit card to filter transactions for (optional).
-     * @param accountId        The ID of the account to filter transactions for (optional).
-     * @param model           The model to be used for rendering the view.
-     * @param session         The HTTP session containing the logged-in user information.
-     * @return The name of the view to be rendered.
-     */
+
+	/**
+	 * This method displays a list of transactions for a given credit card or
+	 * account.
+	 *
+	 * @param month        The month for which to filter transactions (optional).
+	 * @param creditCardId The ID of the credit card to filter transactions for
+	 *                     (optional).
+	 * @param accountId    The ID of the account to filter transactions for
+	 *                     (optional).
+	 * @param model        The model to be used for rendering the view.
+	 * @param session      The HTTP session containing the logged-in user
+	 *                     information.
+	 * @return The name of the view to be rendered.
+	 */
 	@PostMapping("/viewTransactions")
-	public String viewCardTransactions(
-			@RequestParam(name = "month", required = false) String month,
+	public String viewCardTransactions(@RequestParam(name = "month", required = false) String month,
 			@RequestParam(name = "creditCardId", required = false) String creditCardId,
 			@RequestParam(name = "accountId", required = false) String accountId, Model model, HttpSession session) {
 
@@ -90,7 +94,6 @@ public class TransactionController {
 							monthValue, userAccount);
 					Collections.sort(transactions, Comparator.comparing(Transaction::getTransactionDate));
 				}
-
 				model.addAttribute("transactions", transactions);
 				model.addAttribute("account", userAccount);
 
@@ -109,34 +112,39 @@ public class TransactionController {
 				model.addAttribute("creditCard", userCreditCard);
 				model.addAttribute("transactions", transactions);
 			}
+			String currentMonth = Month.of(LocalDateTime.now().getMonthValue()).toString();
+			model.addAttribute("currentMonth", currentMonth);
 			return "view-transactions";
 
 		}
 	}
-	
+
 	@PostMapping("/convertToInstallments")
 	public String convertToInstallments(@RequestParam("transactionId") String transactionId,
-	                                    @RequestParam(name = "creditCardId", required = false) String creditCardId,
-	                                    Model model, HttpSession session) {
+			@RequestParam(name = "creditCardId", required = false) String creditCardId, Model model,
+			HttpSession session) {
 		logger.info("Running convert to installments");
 		CreditCard creditCard = creditCardService.findById(Long.parseLong(creditCardId));
 		Transaction selectedTransaction = transactionService.findById(Long.parseLong(transactionId));
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        LocalDateTime oneMonthLater = currentDateTime.plusMonths(1);
-        LocalDateTime twoMonthsLater = currentDateTime.plusMonths(2);
-        Transaction transaction1 = new Transaction(currentDateTime, "CC Purchase", selectedTransaction.getTransactionAmount()/2, null,
-				0.00, creditCard, null, selectedTransaction.getTransactionMerchantCategoryCode(), selectedTransaction.getTransactionCurrency());
-        Transaction transaction2 = new Transaction(oneMonthLater, "CC Purchase", selectedTransaction.getTransactionAmount()/2, null,
-				0.00, creditCard, null, selectedTransaction.getTransactionMerchantCategoryCode(), selectedTransaction.getTransactionCurrency());
-        Transaction transaction3 = new Transaction(twoMonthsLater, "CC Purchase", selectedTransaction.getTransactionAmount()/2, null,
-				0.00, creditCard, null, selectedTransaction.getTransactionMerchantCategoryCode(), selectedTransaction.getTransactionCurrency());
-	    transaction1.setDescription("1st month Installment, " +  selectedTransaction.getDescription());
-	    transaction2.setDescription("2nd month Installment, " +  selectedTransaction.getDescription());
-	    transaction3.setDescription("3rd month Installment, " +  selectedTransaction.getDescription());
-	    // Redirect the user back to the page displaying the transactions
-	    transactionService.persist(transaction1);
-	    transactionService.persist(transaction2);
-	    transactionService.persist(transaction3);
-	    return "redirect:/viewTransactions";
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		LocalDateTime oneMonthLater = currentDateTime.plusMonths(1);
+		LocalDateTime twoMonthsLater = currentDateTime.plusMonths(2);
+		Transaction transaction1 = new Transaction(currentDateTime, "CC Purchase",
+				selectedTransaction.getTransactionAmount() / 2, null, 0.00, creditCard, null,
+				selectedTransaction.getTransactionMerchantCategoryCode(), selectedTransaction.getTransactionCurrency());
+		Transaction transaction2 = new Transaction(oneMonthLater, "CC Purchase",
+				selectedTransaction.getTransactionAmount() / 2, null, 0.00, creditCard, null,
+				selectedTransaction.getTransactionMerchantCategoryCode(), selectedTransaction.getTransactionCurrency());
+		Transaction transaction3 = new Transaction(twoMonthsLater, "CC Purchase",
+				selectedTransaction.getTransactionAmount() / 2, null, 0.00, creditCard, null,
+				selectedTransaction.getTransactionMerchantCategoryCode(), selectedTransaction.getTransactionCurrency());
+		transaction1.setDescription("1st month Installment, " + selectedTransaction.getDescription());
+		transaction2.setDescription("2nd month Installment, " + selectedTransaction.getDescription());
+		transaction3.setDescription("3rd month Installment, " + selectedTransaction.getDescription());
+		// Redirect the user back to the page displaying the transactions
+		transactionService.persist(transaction1);
+		transactionService.persist(transaction2);
+		transactionService.persist(transaction3);
+		return "redirect:/viewTransactions";
 	}
 }
